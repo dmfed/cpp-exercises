11e98e9bd0e22ff30dc123b7536e2b0e8ebef1a2
dmfed <60199778+dmfed@users.noreply.github.com>
2020-06-27 03:01:30 +0300

    Corrected mistype

--- dec_to_hex_to_oct.cpp
+++ dec_to_hex_to_oct.cpp
@@ -5,7 +5,7 @@ int main() {
 	int number;
 	cout << "Input number: ";
 	cin >> number;
-	cout << "Value if number in hex = 0x"
+	cout << "Value of number in hex = 0x"
 	<< hex << number << endl;
 	cout << "Value in octal = 0"
 	<< oct << number << endl;
